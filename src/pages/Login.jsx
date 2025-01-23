a7b4b022eec85deab2c098e1fa91a44391db7fb5
cwts007 <cthompson@maigas.cl>
2025-01-23 09:49:15 -0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,26 +4,26 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const [loading, setLoading] = useState(false); // Estado para el botón de carga
-    const navigate = useNavigate(); // Navegación programática
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
     const handleLogin = async (values) => {
         setLoading(true);
+        const apiUrl = window.location.hostname === 'localhost'
+            ? 'http://localhost:3000'
+            : 'https://back-mu-ochre.vercel.app';
+
         try {
-            // Realizar la solicitud al backend
-            const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/usuarios/login`, {
+            const response = await axios.post(`${apiUrl}/usuarios/login`, {
                 correo_electronico: values.email,
                 contrasena: values.password,
             });
 
-            // Mostrar mensaje de éxito
-            message.success('Inicio de sesión exitoso');
-            // Guardar el token en LocalStorage
             localStorage.setItem('token', response.data.token);
-            // Redirigir al usuario a la página principal
+            message.success('Inicio de sesión exitoso');
             navigate('/');
         } catch (error) {
-            // Manejo de errores
+            console.error('Error:', error);
             message.error('Credenciales incorrectas');
         } finally {
             setLoading(false);
@@ -35,7 +35,7 @@ const Login = () => {
             <h1>Inicio de Sesión</h1>
             <Form
                 layout="vertical"
-                onFinish={handleLogin} // Manejo del envío del formulario
+                onFinish={handleLogin}
             >
                 <Form.Item
                     label="Correo Electrónico"
